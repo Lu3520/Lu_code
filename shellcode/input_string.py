77cfdb75a6e405e3cde5c7973171fa6c71636207
Lu3520 <75825064+Lu3520@users.noreply.github.com>
2022-03-11 17:25:52 +0900

    Update input_string.py

--- shellcode/input_string.py
+++ shellcode/input_string.py
@@ -1,9 +1,4 @@
-from root_numpy import root2array, tree2array
-from ROOT import TH2D, TH1D
-from root_numpy import fill_hist
-import numpy as np
-
-#input file
+#input string
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('-f', action = 'store', dest='filename', type='string',help='targe host', default = "defult")
